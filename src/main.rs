d3f565a05038004beecb26d47fbb6daa1d39c43e
RicardoCacao <ricpcacao@gmail.com>
2022-07-30 18:56:12 +0100

    First version of vga_buffer

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 #![no_main] // disable all Rust-level entry points
 use core::panic::PanicInfo;
 
-
+mod vga_buffer;
 static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle] // don't mangle the name of this function
