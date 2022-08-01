1c84f93a5887643cbd0125d2e1bc691df880224d
RicardoCacao <ricpcacao@gmail.com>
2022-08-02 00:51:21 +0100

    Implemented double fault expection catch and startit stack overflow testing

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,23 @@ use rust_os::println;
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    rust_os::init();
+    unsafe {
+        *(0xdeadbeef as *mut u64) = 42;
+    };
+
+    x86_64::instructions::interrupts::int3();
+
+    fn stack_overflow() {
+        stack_overflow();
+    }
+
+    stack_overflow();
+
     #[cfg(test)]
     test_main();
 
+    println!("It did not crash!");
     loop {}
 }
 
