915096f12e597539f2a73809400dac8bb3a8d40e
RicardoCacao <ricpcacao@gmail.com>
2022-08-01 22:17:54 +0100

    Integrated tests with should panic tests

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -7,21 +7,19 @@
 use core::panic::PanicInfo;
 use rust_os::println;
 
-
-#[no_mangle]// don't mangle the name of this function
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     test_main();
-    
-    loop{}
+
+    loop {}
 }
-
-
 
 #[test_case]
-fn test_println(){
-println!("test_println output");
+fn test_println() {
+    println!("test_println output");
 }
 
-
 #[panic_handler]
-fn panic(info: &PanicInfo) -> ! { rust_os::test_panic_handler(info) }
+fn panic(info: &PanicInfo) -> ! {
+    rust_os::test_panic_handler(info)
+}
